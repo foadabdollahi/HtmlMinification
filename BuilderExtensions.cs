ad584b5d6194bc4ea8dbc8e912e91c39280be094
Foad abdollahi <foadabdollahi@users.noreply.github.com>
2020-10-19 11:34:38 +0330

    Add files via upload

---
+++ BuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+namespace HtmlMinification
+{
+    public static class BuilderExtensions
+    {
+         
+        public static IApplicationBuilder UseHtmlMinification(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<HtmlMinificationMiddleware>();
+        }
+    }
+}
